f1e9ec72f93b89e8de7d52c30b0d5bff8b5f3029
Louis DeScioli <louis.descioli@gmail.com>
2014-10-30 15:51:35 -0400

    Version format corrected

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
-  summary: "Stripe.js for Meteor",
-  version: "0.1",
+  summary: "Stripe.js packaged for Meteor",
+  version: "0.0.1",
   name: "grove:stripe.js",
   git: "https://github.com/grovelabs/meteor-stripe.js.git"
 });
